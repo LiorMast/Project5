0c29c60a73270d5180ee755eac1a38113d7046e0
Lior Masturov <liormasturov@gmail.com>
2023-01-15 21:51:42 +0200

    added comments

--- מטלה 10/Q9_1.c
+++ מטלה 10/Q9_1.c
@@ -21,63 +21,53 @@ void getUserInput(char* str, int size) {
 	}
 }
 
-/*
-int countWords(char* str) {
-	char* start = str;
-	int wordcount = 0;
-	if (str == start && *str != ' ') wordcount++;
-	while (*str) {
-		if (*str == ' ' && *(str + 1) != '\0')
-			if (*str == ' ' && *(str + 1) != ' ') wordcount++;
-			
-		str++;
-}
-	return wordcount;
-}
-*/
-
+//function that checks if a letter is not in a string of letters
 int isNotInList(char* list, char ltr) {
 	while (*list)
 	{
-		if (*list == ltr) return 0;
+		if (*list == ltr) return 0; //if the letter is found return 0
 		list++;
 	}
-	return 1;
+	return 1; //if it is not found return 1
 }
 
+//function that takes a string and returns a new string that contains every first letter of every word.
+//a letter does not appear in the list more than once.
 char* newStr(char* str) {
-	char* letters = (char*)malloc((31)*sizeof(char));
-	char* start = letters;
-	if(letters){
-		if (*str != ' ') {
+	char* letters = (char*)malloc((31)*sizeof(char)); //allocate an array for the list
+	//(we shrink the array to the desired size after all of the letters have been obtained).
+	char* start = letters; //pointer to the start of the list so we can return it
+	if(letters){ //if the allocation is successful
+		if (*str != ' ') { //take the first letter of the first word if it isn't space
 			*letters = *str;
 			letters++;
 		}
 		while (*str) {
 			if (*str == ' ' && *(str + 1) != '\0')
 				if (*str == ' ' && *(str + 1) != ' ')
-					if(isNotInList(start, *(str + 1))) {
+					if(isNotInList(start, *(str + 1))) { //for every other letter check for a pair of a space and a non-space
+														//and check if we already have the non-space letter in the list.
 					*letters = *(str + 1);
 					letters++;
 				}
 
 			str++;
 		}
-		*letters = '\0';
+		*letters = '\0'; //add \0 at the end of the list to turn it to a string
 	}
 	else printf("Out of memory!\n");
-	if(start) start = realloc(start, (strlen(start) + 1) * sizeof(char));
-	return start;
+	if(start) start = realloc(start, (strlen(start) + 1) * sizeof(char)); //shrink the size of the string to perfectly fit the string size
+	return start; //return the list
 }
 
 
 
 void main() {
-	char* ptrstr = NULL;
-	char str[SIZE];
-	getUserInput(str, SIZE);
-	ptrstr = newStr(str);
-	printf("%s\n", ptrstr);
-	free(ptrstr);
+	char* ptrstr = NULL; //pointer to hold the list
+	char str[SIZE]; //string to hold user input
+	getUserInput(str, SIZE); //get user input to string
+	ptrstr = newStr(str); //get the list
+	printf("%s\n", ptrstr);//display the list
+	free(ptrstr); //free the list from memory after we're done with it
 	
 }
